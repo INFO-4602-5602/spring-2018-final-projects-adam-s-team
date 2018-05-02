395fd90613c5dfe93b1b9cd333c370ce09c98462
Stephen Smart <stephendsmart@gmail.com>
2018-05-02 14:03:44 -0600

    Made some changes to vis2 and vis3.

--- vis3/data/raw_data/process_vis3_data.py
+++ vis3/data/raw_data/process_vis3_data.py
@@ -16,7 +16,15 @@ subreddit_5_lines = file.readlines()[1:]
 file = open('ask_women.csv', 'r')
 subreddit_6_lines = file.readlines()[1:]
 
-subreddit_lines = [subreddit_1_lines, subreddit_2_lines, subreddit_3_lines, subreddit_4_lines, subreddit_5_lines, subreddit_6_lines]
+file = open('ask_culinary.csv', 'r')
+subreddit_7_lines = file.readlines()[1:]
+
+file = open('ask_anthropology.csv', 'r')
+subreddit_8_lines = file.readlines()[1:]
+
+subreddit_lines = [subreddit_1_lines, subreddit_2_lines, subreddit_3_lines, 
+				   subreddit_4_lines, subreddit_5_lines, subreddit_6_lines, 
+				   subreddit_7_lines, subreddit_8_lines]
 
 max_karma = 51
 for i in range(max_karma):
@@ -95,5 +103,29 @@ for i in range(max_karma):
 			below = below + 1
 
 	out_file.write('Ask_Women,' + str(above) + ',' + str(below) + '\n')
+
+	above = 0
+	below = 0
+	for line in subreddit_7_lines:
+		line = line.replace(' ', '').replace('\n', '').split(',')
+		karma_value = int(line[3])
+		if karma_value >= i:
+			above = above + 1
+		else:
+			below = below + 1
+
+	out_file.write('Ask_Culinary,' + str(above) + ',' + str(below) + '\n')
+
+	above = 0
+	below = 0
+	for line in subreddit_8_lines:
+		line = line.replace(' ', '').replace('\n', '').split(',')
+		karma_value = int(line[3])
+		if karma_value >= i:
+			above = above + 1
+		else:
+			below = below + 1
+
+	out_file.write('Ask_Anthropology,' + str(above) + ',' + str(below) + '\n')
 
 	out_file.close()
